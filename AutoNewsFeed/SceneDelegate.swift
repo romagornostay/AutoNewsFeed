85a2101f17c75cb78161d8bc291dda96b3ba701c
Roman Gornostayev <roman.gornostayev@yandex.ru>
2025-06-29 23:18:25 +0800

    move code to SPM

---
+++ AutoNewsFeed/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  AutoNewsFeed
+//
+//  Created by Roman Gornostayev on 20.06.2025.
+//
+
+import App
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+  var window: UIWindow?
+  private var appCoordinator: AppCoordinator?
+
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+    
+    let window = UIWindow(windowScene: windowScene)
+    let appCoordinator = AppCoordinator(window: window)
+    self.window = window
+    self.appCoordinator = appCoordinator
+    appCoordinator.start()
+  }
+}
+
